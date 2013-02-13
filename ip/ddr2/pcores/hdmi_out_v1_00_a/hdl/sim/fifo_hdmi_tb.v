6470534e7849786eebcc494de1ddf76b55ca17c3
Ana <ana.klimov@gmail.com>
2013-02-12 22:49:00 -0500

    Testbench for hdmi and fifo fsm. Also instatiated xilinx fifo...we will need to see if it compiles (ie: if Modelsim has the fifo module in a library).

--- ip/ddr2/pcores/hdmi_out_v1_00_a/hdl/sim/fifo_hdmi_tb.v
+++ ip/ddr2/pcores/hdmi_out_v1_00_a/hdl/sim/fifo_hdmi_tb.v
@@ -4,21 +4,35 @@ module fifo_hdmi_tb (
 );
 
   parameter FRAME_BASE_ADDR = 32'h80000000;
-  parameter LINE_STRIDE = 32'h00004000;
-  parameter NUM_PIXELS_PER_LINE = 32'h00000100;
-  parameter NUM_BYTES_PER_PIXEL = 32'h00000004;
+  parameter LINE_STRIDE = 32'd1280;
+  parameter NUM_PIXELS_PER_LINE = 32'd64;
+  parameter NUM_BYTES_PER_PIXEL = 32'd4;
   
   wire [31:0] ddr_addr_to_read;
-  wire go_fill_fifo;
+  wire fifo_write_go;
   
-  reg clk, reset, start;
-  reg fill_half_fifo_I;
-  reg hsync_I, vsync_I;
+  reg clk, reset_fifo, start_fifo;
+  reg reset_hdmi, start_hdmi;
+  
+  wire                                      hsync;
+  wire                                      vsync;
+  wire                                      ve;
+  wire                                      read_go;
+  wire                                      read_next_line;
+  wire                                      read_next_chunk;
+  wire                                      read_done;
 
+ wire [31:0] fifo_out_data; //output data of fifo
+ 
+ reg [31:0] fifo_in_data;  //data to write to fifo
+ reg mst_fifo_valid_write_xfer; //tell fifo to start writing data 
+ reg bus2ip_Reset; //reset for xilinx fifo
+ 
+  
 fill_fifo_fsm fill_fifo(
 			.Bus2IP_Clk(clk),
 			.reset_fill_fifo(reset_fifo),	
-			.start_fill_fifo(start_fifo),
+			.start_fill_fifo(start_fifo | read_go),
 			.hsync(read_next_line),			//obtain hsync and vsync from hdmi_core
 			.vsync(read_done),
 			.half_full(read_next_chunk),
@@ -31,7 +45,7 @@ fill_fifo_fsm fill_fifo(
 		      	);		
 			
 
-
+/*
 //this module stimulates signals because fill_fifo_fsm expects hsync, vsync, and half_full to last only one clk cycle
 pulse_gen #(3) stimulate_signals4_fifo_fsm(
   			.clk(clk),
@@ -40,17 +54,18 @@ pulse_gen #(3) stimulate_signals4_fifo_fsm(
 			.posedge_O(),
 			.negedge_O()
 			);
+*/
 
   hdmi_core hdmi_core_inst (
     .reset(reset_hdmi),
     .start(start_hdmi),
-    .clock(PXL_CLK_X1),
+    .clock(clk), //note: assuming same clock for bus2ip and PXL_CLK_X1!!! is that a valid assumption...?
     .hres(11'd1280),
     .vres(10'd720),
-    .color(ip2bus_mstwr_d /*slv_reg1[23:0]*/),
-    .red(red),
-    .blue(blue),
-    .green(green),
+    .color(fifo_out_data /*slv_reg1[23:0]*/),
+    .red(),
+    .blue(),
+    .green(),
     .hsync(hsync),
     .vsync(vsync),
     .read_go(read_go),
@@ -62,15 +77,15 @@ pulse_gen #(3) stimulate_signals4_fifo_fsm(
  
   // FIFO depth is 128 words. User can modify the depth based on their requirement.
    srl_fifo_f #(
-     .C_DWIDTH(C_MST_NATIVE_DATA_WIDTH),
+     .C_DWIDTH(32),
      .C_DEPTH(128))
    DATA_CAPTURE_FIFO_I (
      .Clk(clk),
      .Reset(bus2ip_Reset),
-     .FIFO_Write(mst_fifo_valid_write_xfer),
-     .Data_In(bus2ip_mstrd_d),
-     .FIFO_Read(ve/*mst_fifo_valid_read_xfer*/),
-     .Data_Out(ip2bus_mstwr_d),
+     .FIFO_Write(mst_fifo_valid_write_xfer | fifo_write_go), //note: fifo_write_go is not used in this way in user_logic
+     .Data_In(fifo_in_data),
+     .FIFO_Read(ve /*mst_fifo_valid_read_xfer*/),
+     .Data_Out(fifo_out_data),
      .FIFO_Full(),
      .FIFO_Empty(),
      .Addr()); // DATA_CAPTURE_FIFO_I
@@ -83,80 +98,42 @@ pulse_gen #(3) stimulate_signals4_fifo_fsm(
   initial
   begin
     @ (negedge clk)
-	reset = 1;
-	start = 0;
-    fill_half_fifo_I = 0;
-    hsync_I = 0;
-    vsync_I = 0;
+	reset_fifo = 1;
+	start_fifo = 0;
+    reset_hdmi = 1;
+    start_hdmi = 0;
+    mst_fifo_valid_write_xfer = 1; //give fifo head start to fill up data
+	fifo_in_data = 32'h000000ff;
+	bus2ip_Reset = 0;
     @ (negedge clk) ;
-	reset = 0;
-	start = 1;
-    @ (negedge clk) ;
-    @ (negedge clk) ;
-    @ (negedge clk) ;
-    @ (negedge clk) ;
-    @ (negedge clk)
-    fill_half_fifo_I = 1;
-    @ (negedge clk)
-    fill_half_fifo_I = 0;
-    @ (negedge clk)
-    fill_half_fifo_I = 1;
-    @ (negedge clk)
-    fill_half_fifo_I = 0;
-    @ (negedge clk)
-    fill_half_fifo_I = 1;
-    @ (negedge clk)
-    fill_half_fifo_I = 0;
+	reset_fifo = 0;
+	reset_hdmi = 0;
+	mst_fifo_valid_write_xfer = 0; //does this signal need to stay high? probably not (we can test this)
     @ (negedge clk) ;
     @ (negedge clk) ;
-    @ (negedge clk)
-    hsync_I = 1;
-    @ (negedge clk)
-    hsync_I = 0;
+    @ (negedge clk) ;
+	@ (negedge clk) ;
+	@ (negedge clk) ;
+    @ (negedge clk) ;
+    @ (negedge clk) ;
+	start_fifo = 1;					//start fifo fsm
+	fifo_in_data = 32'h0000ff00;	//new data ready
+	start_hdmi = 1;
+    @ (negedge clk) ;
+	start_fifo = 0;					//this should not need to stay high
+	start_hdmi = 0;					//what about start signal for hdmi?
     @ (negedge clk) ;
     @ (negedge clk) ;
     @ (negedge clk) ;
     @ (negedge clk) ;
-    @ (negedge clk)
-    fill_half_fifo_I = 1;
-    @ (negedge clk)
-    fill_half_fifo_I = 0;
-    @ (negedge clk)
-    fill_half_fifo_I = 1;
-    @ (negedge clk)
-    fill_half_fifo_I = 0;
-    @ (negedge clk)
-    fill_half_fifo_I = 1;
-    @ (negedge clk)
-    fill_half_fifo_I = 0;
     @ (negedge clk) ;
-    @ (negedge clk) ;
-    @ (negedge clk)
-    hsync_I = 1;
-    @ (negedge clk)
-    hsync_I = 0;
+	fifo_in_data = 32'h00ff0000;	//new data available to write to fifo
+	@ (negedge clk) ;
     @ (negedge clk) ;
     @ (negedge clk) ;
     @ (negedge clk) ;
     @ (negedge clk) ;
-    @ (negedge clk)
-    fill_half_fifo_I = 1;
-    @ (negedge clk)
-    fill_half_fifo_I = 0;
-    @ (negedge clk)
-    fill_half_fifo_I = 1;
-    @ (negedge clk)
-    fill_half_fifo_I = 0;
-    @ (negedge clk)
-    fill_half_fifo_I = 1;
-    @ (negedge clk)
-    fill_half_fifo_I = 0;
-    @ (negedge clk) ;
-    @ (negedge clk) ;
-    @ (negedge clk)
-    vsync_I = 1;
-    @ (negedge clk)
-    vsync_I = 0;
+	fifo_in_data = 32'h000000aa;
   end
 
 endmodule
