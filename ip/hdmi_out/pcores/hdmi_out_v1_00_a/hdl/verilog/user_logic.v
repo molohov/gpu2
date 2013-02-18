a77761ebbfa70b3ca822eb9066964c244d0d9c84
Ana Klimovic <ana.klimov@gmail.com>
2013-02-18 16:05:37 -0500

    Corrected HRES for 640x480

--- ip/hdmi_out/pcores/hdmi_out_v1_00_a/hdl/verilog/user_logic.v
+++ ip/hdmi_out/pcores/hdmi_out_v1_00_a/hdl/verilog/user_logic.v
@@ -110,7 +110,7 @@ module user_logic
 
 // -- ADD USER PARAMETERS BELOW THIS LINE ------------
 // --USER parameters added here 
-parameter HDMI_HRES                      = 1280;
+parameter HDMI_HRES                      = 640;
 parameter HDMI_BYTES_PER_PIXEL           = 4;
 // -- ADD USER PARAMETERS ABOVE THIS LINE ------------
 
