ffa8db1ba3bfe8c1355e070e1603fc90a8301933
Bryce Long <molohov@gmail.com>
2013-03-06 14:28:25 -0500

    Modified zbuff test case again. Fails with painter's algorithm.

--- project/software/zbuff.c
+++ project/software/zbuff.c
@@ -5,56 +5,37 @@
 int main()
 {
   gpSetBackgroundColor(0x60, 0x00, 0xe0);
-
-  // Create this scene to test depth. It would fail the painter's algorithm:
-  // NOTE: diagonal square is actually a triangle, but the square is shown to illustrate the angle relative to the camera-planar square
-  /*
-  |\
-  | \------
-  |  |    |
-   \ |    |
-    \|-----
-    */
   
-  gpPoly *tri1 = gpCreatePoly(3);
-  gpSetPolyVertex(tri1, 0, 0.f, 0.f, 0.f);
-  gpSetPolyVertex(tri1, 1, -1.f, 0.f, 1.f);
-  gpSetPolyVertex(tri1, 2, -1.f, 1.f, 1.f);
-  gpSetPolyColor(tri1, 0x0, 0xff, 0x0); //green
-
-  gpPoly *tri2 = gpCreatePoly(3);
-  gpSetPolyVertex(tri2, 0, 0.f, 0.f, 0.f);
-  gpSetPolyVertex(tri2, 1, 1.f, 0.f, 1.f);
-  gpSetPolyVertex(tri2, 2, 1.f, 1.f, 1.f);
-  gpSetPolyColor(tri2, 0x0, 0xff, 0x0); //green
-
   gpPoly *quad1 = gpCreatePoly(4);
-  gpSetPolyVertex(quad1, 0, -0.25f, -0.5f, 0.f);
-  gpSetPolyVertex(quad1, 1, -0.25f, 0.25f, 0.f);
-  gpSetPolyVertex(quad1, 2, 0.25f, 0.25f, 0.f);
-  gpSetPolyVertex(quad1, 3, 0.25f, -0.5f, GP_INFER_COORD);
-  gpSetPolyColor(quad1, 0xff, 0x0, 0x0); //blue
+  gpSetPolyVertex(quad1, 0, 1.f, 1.f, 0.f);
+  gpSetPolyVertex(quad1, 1, 1.f, 0.f, 0.f);
+  gpSetPolyVertex(quad1, 2, -1.f, 0.f, 0.f);
+  gpSetPolyVertex(quad1, 3, -1.f, 1.f, GP_INFER_COORD);
+  gpSetPolyColor(quad1, 0x0, 0xff, 0x0); //green
 
   gpPoly *quad2 = gpCreatePoly(4);
-  gpSetPolyVertex(quad2, 0, -0.5f, 0.f, 0.5f);
-  gpSetPolyVertex(quad2, 1, -0.5f, 1.f, 0.5f);
-  gpSetPolyVertex(quad2, 2, 0.5f, 1.f, 0.5f);
-  gpSetPolyVertex(quad2, 3, 0.5f, 0.f, GP_INFER_COORD);
+  gpSetPolyVertex(quad2, 0, 0.f, 0.f, 0.f);
+  gpSetPolyVertex(quad2, 1, 0.f, 1.f, 0.f);
+  gpSetPolyVertex(quad2, 2, 1.f, 1.f, 0.f);
+  gpSetPolyVertex(quad2, 3, 1.f, 0.f, GP_INFER_COORD);
   gpSetPolyColor(quad2, 0x0, 0x0, 0xff); //blue
 
   gpPolyList *list = gpCreatePolyList();
-  gpAddPolyToList(list, quad1);
   gpAddPolyToList(list, quad2);
-  gpAddPolyToList(list, tri1);
-  gpAddPolyToList(list, tri2);
+  gpAddPolyToList(list, quad1);
 
-  gpTranslatePolyList(list, 0.0, -0.5, 2.0);
+  gpTranslatePolyList(list, 0.0, -1.0, 2.0);
 
   gpEnable(GP_ZBUFFER);
   gpEnable(GP_PERSPECTIVE);
-  gpSetFrustrum(1.0, 3.0);
+  gpSetFrustrum(1.0, 4.0);
 
   gpRender(list);
+
+  while (1) {
+      gpRotatePoly(quad1, 0.0, 0.1, 0.0);
+      gpRender(list);
+  }
 
   gpDisable(GP_PERSPECTIVE);
   gpRender(list);
