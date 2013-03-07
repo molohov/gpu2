e85dca5dbe465f3d05c13a4838e62b7542f67059
Victor Zhang <stetorvs@gmail.com>
2013-03-07 08:07:37 -0500

    Don't think we need to debug more.

--- project/software/gp.c
+++ project/software/gp.c
@@ -306,12 +306,7 @@ void gpFillPoly(gpPoly *poly, gpImg *img)
             vertices[i].z = (unsigned)(((GLOBAL_FAR + GLOBAL_NEAR)/(2*(GLOBAL_FAR - GLOBAL_NEAR)) + (1/poly->t_vertices[i].z)*(-GLOBAL_FAR*GLOBAL_NEAR)/(GLOBAL_FAR - GLOBAL_NEAR) + 1/2) * GLOBAL_ZBUFFER_MAX);
           else
             vertices[i].z = (unsigned)(poly->t_vertices[i].z * GLOBAL_ZBUFFER_MAX);
-          /* DEBUG
-          printf("floating point: x: %f, y: %f, z: %f\n", poly->t_vertices[i].x, poly->t_vertices[i].y, poly->t_vertices[i].z);
-          printf("fixed point: x: %d, y: %d, z: %x\n", vertices[i].x, vertices[i].y, vertices[i].z);
-          */
         }
-        // DEBUG printf("end poly\n");
 
         gpFillConvexPolyZBuff(img, vertices, poly->num_vertices, &poly->color);
 
