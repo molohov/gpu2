6654675062b01fdd7f09a3b9eb2285ee1d1ada0b
Bryce Long <molohov@gmail.com>
2013-03-11 11:22:41 -0400

    First revision of fsm for hline zbuff fill.

--- project/hardware/pcores/hline_zbuff_v1_00_a/hdl/verilog/user_logic.v
+++ project/hardware/pcores/hline_zbuff_v1_00_a/hdl/verilog/user_logic.v
@@ -312,31 +312,6 @@ input                                     bus2ip_mstwr_dst_dsc_n;
   assign    z2         = slv_reg6;
   assign    rgbx       = slv_reg7;
 
-  // begin state machine that controls fb writing and zbuff comparison. in pseudocode:
-  // 1. burst the maximum length (256 words) into a FIFO that is also 256 words long
-  // 2. from x1 to x2
-  //    2a. calculate the interpolated z value for the current x value 
-  //    2b. check to see if the calculated z value is less than (therefore in front of) the existing z value
-  //    2c. if it is, update the z-write FIFO as well as the byte-enable FIFO (which represents replaced or not replaced)
-  // 3. burst write the z-line and use the byte-enable FIFO as byte-enables
-  // 4. burst write the fb to the appropriate address (using the rgbx value) and reuse the byte-enable FIFO 
-  // 5. subtract 256 from the length of the line = abs(x1 - x2)
-  // 5. repeat until length of line = 0 
-
-  // guessing how many states there might be
-  reg [2:0] state, nextstate; 
-  reg [3:0] be, nextbe;
-  reg [1:0] beindex, nextbeindex;
-
-  localparam    IDLE        = 3'd0,  // reset state. calculate the 
-                LOAD_ZBUFF  = 3'd1,  // load zbuff MAX_LEN from pre calculated address and set AXI burst params accordingly
-                TRAVERSE_X  = 3'd2,
-                INTERP_Z    = 3'd3, // who knows how long this may take...assume it can calculate and write values in the same state (1 z-value and 1 byte-enable per 4 cycles)
-                WR_ZBUFF    = 3'd4, // burst the zbuffer and wait for completion
-                WR_FBUFF    = 3'd5; // burst the framebuffer and wait for completion. calculate the next burst addr
-
-  // STATE MACHINE GOES HERE
-
 
   // ------------------------------------------------------
   // Example code to read/write user logic slave model s/w accessible registers
