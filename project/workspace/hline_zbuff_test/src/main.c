39a8932ae800360c7010278ef28e0c250ba15fb8
Bryce Long <molohov@gmail.com>
2013-03-15 23:51:45 -0400

    Software test for hline zbuff pcore. Failing right now. If this is wrong location, feel free to move.

---
+++ project/workspace/hline_zbuff_test/src/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "xparameters.h"    /* generated system parameters                  */
+#include "xbasic_types.h"   /* Xilinx basic types for device drivers        */
+#include "xutil.h"
+
+#define printf xil_printf   /* a smaller footprint printf */
+
+int main (void)
+{
+	int * hline_pcore = (int *)XPAR_HLINE_ZBUFF_0_BASEADDR;
+	int * fbuff = (int *)0xAC000000;
+	int * zbuff = fbuff + 1024;
+
+	int i = 0;
+	while (i < 1024) {
+		fbuff[i] = 0;
+		zbuff[i] = 0xffffffff;
+		i++;
+	}
+
+	hline_pcore[0] = (int)fbuff;
+	hline_pcore[1] = (int)zbuff;
+	hline_pcore[3] = 256;
+	hline_pcore[4] = 0;
+	hline_pcore[5] = 0x00ffffff;
+	hline_pcore[6] = 0xdeadbeef;
+	hline_pcore[7] = 128;
+	hline_pcore[8] = 255;
+	// start it
+
+	hline_pcore[11] = 1;
+
+	return 0;
+}
