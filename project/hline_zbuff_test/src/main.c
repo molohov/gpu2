325d873db0fafabb40a4cd701ac02fa2850cefcb
Bryce Long <molohov@gmail.com>
2013-03-18 16:45:40 -0400

    Minor updates to test program.

--- project/hline_zbuff_test/src/main.c
+++ project/hline_zbuff_test/src/main.c
@@ -10,10 +10,10 @@ int main ()
 {
 	volatile int * hline_pcore = (int *)XPAR_HLINE_ZBUFF_0_BASEADDR;
 	char * hline_offset = (char *)(hline_pcore + 64);
-	unsigned int * fbuff = (unsigned int *)0xAC000000;
-	unsigned int * zbuff = fbuff + 1024;
+	volatile unsigned int * fbuff = (volatile unsigned int *)0xAB000000;
+	volatile unsigned int * zbuff = fbuff + 1024;
 
-	printf("%x, %x", (int)fbuff, (int)zbuff);
+	printf("%x, %x\n\r", (int)fbuff, (int)zbuff);
 
 	int i = 0;
 	while (i < 256) {
@@ -24,7 +24,7 @@ int main ()
 
 	hline_pcore[0] = (int)fbuff;
 	hline_pcore[1] = (int)zbuff;
-	hline_pcore[2] = 512; //dx
+	hline_pcore[2] = 256; //dx
 	hline_pcore[3] = 0;  //z1
 	hline_pcore[4] = 0x00ffffff; //slope
 	hline_pcore[5] = 0xdeadbeef; //rgbx
@@ -33,7 +33,6 @@ int main ()
 	// start it
 
 	hline_pcore[11] = 1;
-	i = 0;
 
 	volatile char * debug = (char *)(hline_pcore + 9);
 	printf("fifo head: %x  | ", hline_pcore[8]);
@@ -44,6 +43,6 @@ int main ()
 	printf("status reg: %x | ", hline_offset[1]);
 	printf("bus2ip_mstrd: %x | ", hline_pcore[0]);
 	printf("mst_fifo_valid: %x | ", hline_pcore[1]);
-	printf("ip2bus_mstwr: %x | ", hline_pcore[2]);
+	printf("ip2bus_mstwr: %x\n\r", hline_pcore[2]);
 	return 0;
 }
