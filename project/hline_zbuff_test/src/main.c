99d1a0f50aee4e3503526813bf448233e41b9213
Bryce Long <molohov@gmail.com>
2013-03-23 17:07:17 -0400

    Updated hline_zbuff test and parametrized the test criteria.

--- project/hline_zbuff_test/src/main.c
+++ project/hline_zbuff_test/src/main.c
@@ -5,73 +5,38 @@
 #include "xutil.h"
 
 #define printf xil_printf   /* a smaller footprint printf */
-#define BURST_TEST
+#define HLINE_TEST
+#define LENGTH 256 //multiple of 256, up to the size of buffer regions (BUF_SIZE)
+#define BUF_SIZE 1024
 
 int main ()
 {
 	volatile unsigned int * fbuff = (volatile unsigned int *)0xAB000000;
-	volatile unsigned int * zbuff = fbuff + 1024;
+	volatile unsigned int * zbuff = fbuff + BUF_SIZE;
 
 	int i = 0;
-	while (i < 256) {
+	while (i < BUF_SIZE) {
 		fbuff[i] = i;
 		zbuff[i] = 0xffffffff-i;
 		i++;
 	}
 
-#ifdef BURST_TEST
-	volatile unsigned int * burst_write_addr = (unsigned int *)XPAR_BURST_WRITE_0_BASEADDR;
-
-	// burst read
-	burst_write_addr[64] = 1 | (1 << 3); // burst read
-	burst_write_addr[65] = 0xab000000; //address
-	burst_write_addr[66] = 0xffff; // byte enable
-    burst_write_addr[67] = 0x0a000000 | 16 * 4; // go and length (16 words)
-    while (!(burst_write_addr[64] & 0x100)) {
-      }
-    burst_write_addr[64] &= ~0x100;
-    printf("read complete!\n\r");
-
-	// burst write
-	burst_write_addr[0] = 0xdeadbeef; //write value
-	burst_write_addr[64] = (1 << 1) | (1 << 3); // burst write
-	burst_write_addr[65] = 0xab000000; // address
-    burst_write_addr[66] = 0xffff; // byte enable
-    burst_write_addr[67] = 0x0a000000 | 16 * 4; // go and length (16 words)
-    while (!(burst_write_addr[64] & 0x100)) {
-      }
-    // clear done bit
-    burst_write_addr[64] &= ~0x100;
-    printf("write complete!\n\r");
-#endif
-
-#ifdef HLINE_TEST
 	volatile int * hline_pcore = (int *)XPAR_HLINE_ZBUFF_0_BASEADDR;
 	char * hline_offset = (char *)(hline_pcore + 64);
 
-
 	hline_pcore[0] = (int)fbuff;
 	hline_pcore[1] = (int)(zbuff);
-	hline_pcore[2] = 256; //dx
+	hline_pcore[2] = LENGTH; //dx
 	hline_pcore[3] = 0;  //z1
-	hline_pcore[4] = 0x00ffffff; //slope
-	hline_pcore[5] = 0xdeadbeef; //rgbx
-	hline_pcore[6] = 128; //error
-	hline_pcore[7] = 255; //remainder
-	// start it
+	hline_pcore[4] = 0xFFFFFFFF/LENGTH; //slope
+	hline_pcore[5] = 0xbeefbeef; //rgbx
+	hline_pcore[6] = LENGTH/2; //error
+	hline_pcore[7] = LENGTH-1; //remainder
 
-	hline_pcore[11] = 1;
 
-	// poll for completeness
-	// while (hline_pcore[7] == 0);
-	volatile int j = 0;
-	while (j < 2048)
-		j++;
-
-	//while (1) {
 	volatile char * debug = (char *)(hline_pcore + 10);
 	printf("curr_state: %x | ", debug[0]);
-	printf("mst_cmd_sm_rd_req: %x | ", debug[1]);
+	printf("started: %x | ", debug[1]);
 	printf("mst_cmd_sm_wr_req: %x || ", debug[2]);
 	printf("mst_sm_state: %x | ", debug[3]);
 
@@ -80,8 +45,28 @@ int main ()
 	printf("f_fifo_in: %d | ", hline_pcore[9]);
 	printf("ip2bus_mstwr_d: %x | ", hline_pcore[2]);
 	printf("addr: %x\n\r", hline_pcore[11]);
-	//}
-#endif
+
+	getchar();
+	// start the pcore
+	hline_pcore[11] = 1;
+	// turn the start signal off (for some reason the pulsing isn't working in the user_logic)
+	hline_pcore[11] = 0;
+
+	// poll for completeness
+	while (hline_pcore[7] == 0);
+
+	printf("curr_state: %x | ", debug[0]);
+	printf("started: %x | ", debug[1]);
+	printf("mst_cmd_sm_wr_req: %x || ", debug[2]);
+	printf("mst_sm_state: %x | ", debug[3]);
+
+	printf("status reg: %x | ", hline_offset[1]);
+	printf("z_fifo_in: %x | ", hline_pcore[8]);
+	printf("f_fifo_in: %x | ", hline_pcore[9]);
+	printf("ip2bus_mstwr_d: %x | ", hline_pcore[2]);
+	printf("addr: %x\n\r", hline_pcore[11]);
+
+	printf("hline_zbuff test complete!\r\n");
 	return 0;
 }
 
